8b98687779de5990653b518cd5ccd8c414b7e368
Taake <trollfrost666@gmail.com>
2022-12-04 20:41:43 +0300

    Second

--- src/Miles.java
+++ src/Miles.java
@@ -1,11 +1,10 @@
 public class Miles {
     public static void main(String[] args) {
 
-        int price = 20000; // стоимость билета
-        int bonus = 20; // рублей за милю
-        int miles = price / bonus; // мили
-
-        System.out.println("Ваши бонусные мили: " + miles );
+        BonusMilesService service = new BonusMilesService();
+        int price = 20_000;
+        int miles = service.calculate(price);
+        System.out.println( "Ваши бонусные мили: " + miles);
 
 
     }
